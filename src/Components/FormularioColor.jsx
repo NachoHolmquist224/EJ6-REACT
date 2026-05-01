c3328f30d76cf93cf45137645a7ea4a096e92b13
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-05-01 16:39:36 -0300

    agregamos la logica para guardar el array en el local storage y del submit

--- src/Components/FormularioColor.jsx
+++ src/Components/FormularioColor.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { use } from "react";
+import { useState, useEffect } from "react";
 
 const FormularioColor = () => {
-  return (
-    <section>
-      <form>
-        <div className="d-flex gap-2 align-items-center">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Ingrese un color en inglés o su código hexadecimal"
-            required
-          />
-          <button className="btn btn-primary">Agregar</button>
-        </div>
-      </form>
-    </section>
-  );
+  const arrayColoresLocalStorage =
+    JSON.parse(localStorage.getItem("colores")) || [];
+  const [arrayColores, setArrayColores] = useState(arrayColoresLocalStorage);
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("colores", JSON.stringify(arrayColores));
+  }, [arrayColores]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //validamos el color
+    const colorBuscado = arrayColores.find(
+      (itemColor) => itemColor.toLowerCase() === color.toLowerCase().trim(),
+    );
+    console.log(colorBuscado);
+
+    if (colorBuscado) {
+      return alert("El color ingresado ya existe");
+    }
+    //agregar la "color" dentro del arrayColores
+    setArrayColores([...arrayColores, color.trim()]);
+    //limpiar el input
+    setColor("");
+
+    return (
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div className="d-flex gap-2 align-items-center">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Ingrese un color en inglés o su código hexadecimal"
+              required
+            />
+            <button className="btn btn-primary">Agregar</button>
+          </div>
+        </form>
+      </section>
+    );
+  };
 };
 
 export default FormularioColor;
